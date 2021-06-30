5acc55e8178675120dcfd21602650742b44ce6ac
teru-410 <keito.1213.tm@gmail.com>
2021-06-30 14:08:08 +0900

    login

--- pblapp/resources/views/layout/teacher_temp.blade.php
+++ pblapp/resources/views/layout/teacher_temp.blade.php
@@ -1,17 +1,7 @@
 <html>
 <head>
   <title>@yield('title')</title>
-  <style>
-  body {font-size:16pt; color:#999; }
-  h1 { font-size:100pt; text-align:right; color:#f6f6f6;
-      margin:-50px 0px -100px 0px; }
-  .header p {
-    position: absolute;
-    top: -20;
-    left: 20;
-    font-size:70pt;
-    color:#ffffff;}
-  </style>
+  <link rel="stylesheet" href="../resources/css/teacher.css">
 </head>
 <body>
   <div class = "header">
--- pblapp/resources/views/teacher/login.blade.php
+++ pblapp/resources/views/teacher/login.blade.php
@@ -1,6 +1,9 @@
 @extends('layout.teacher_temp')
 @section('title','login')
 @section('content')
-  <p>コンテンツです</p>
+  <form method="post" action="" class="login">
+  <p>UserID :<input type="text" name="userid" value=""></p>
+  <p>Password :<input type="text" name="password" value=""></p>
+  <input type="submit" value="login">
 @endsection
 @section('footer','PBL7')
