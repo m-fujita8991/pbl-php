869847c40276723eecd3102a539eb0c3027ffd36
Shimoyama <shimoyama0909@gmail.com>
2021-07-03 00:01:18 +0900

    no message

--- pblapp/resources/views/student/sankahyou.blade.php
+++ pblapp/resources/views/student/sankahyou.blade.php
@@ -1,12 +1,17 @@
 @extends('layout.student_temp')
 @section('title','参加票　入力画面')
 @section('content')
-<header>
-    <form>
-        <p style="font-size:60px; text-align:center; color:#000000"><br>
-        <p>参加者No<input type="text" name="name" value="" style="font-size:60px; text-align:center; color:#000000"></p>
-    </form>
-</header>
+<body>
+        <p style="font-size:50px; text-align: center; color:#000000;">参加者No<input type="text" name="name" value="" style="font-size:60px; color:#000000"></p> 
+        <p style="font-size:30px; text-align: rigth; color:#000000; letter-spacing: 1em;">名前<input type="text" name="name" value="" style="font-size:40px; color:#000000 "></p>
+        <p style="font-size:30px; text-align: rigth; color:#000000;">ふりがな<input type="text" name="name" value="" style="font-size:40px; color:#000000"></p>
+        <p style="font-size:30px; text-align: rigth; color:#000000; letter-spacing: 0.33em;">学校名<input type="text" name="name" value="" style="font-size:40px; color:#000000"></p>
+        <p style="font-size:30px; text-align: rigth; color:#000000; letter-spacing: 1em;">学年<input type="text" name="name" value="" style="font-size:40px; color:#000000"></p>
+        <p style="font-size:30px; text-align: rigth; color:#000000;">生年月日<input type="text" name="name" value="" style="font-size:40px; color:#000000"></p>
+        <p style="font-size:30px; text-align: left; color:#000000;">既卒<input type="text" name="name" value="" style="font-size:40px; color:#000000"></p>  
+        
+
+</body>
 @endsection 
 @section('footer')
 <footer>
