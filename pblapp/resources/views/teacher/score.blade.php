c1ed8c94a4657b91c243145022c8689209f35c68
teru-410 <keito.1213.tm@gmail.com>
2021-07-08 12:00:38 +0900

    confirm sorce更新

--- pblapp/resources/views/teacher/score.blade.php
+++ pblapp/resources/views/teacher/score.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <p>　　名前 <input type="text" name="name" value=""></p>
     <p>　　参加者メモ</p>
-    <textarea name="note"></textarea>
+    <textarea name="note" style="width:400px; height:200px; font-size:25px;"></textarea>
     <p>1. 　　　　　　　　　　<select name="item1">
         <option value='score1'>2</option>
         <option value='score2'>4</option>
@@ -35,11 +35,11 @@
         <option value='score4'>8</option>
     </select></p>
     <p>不合格の理由を記入してください</p>
-    <textarea name="reason"></textarea>
+    <textarea name="reason" style="width:500px; height:200px; font-size:25px;"></textarea>
 @endsection
 @section('footer','')
 <footer>
    <div class = 'footer'></div>
-   <button onclick="history.back()" >戻る</button>
-   <button>決定</button>
+   <button onclick="history.back()" style="position: absolute; left: 75%; top: 80%; width:75px; height:35px; font-size:20px;">戻る</button>
+   <button onclick="location.href='list'" style="position: absolute; left: 85%; top: 80%; width:75px; height:35px; font-size:20px;">決定</button>
 </footer>
